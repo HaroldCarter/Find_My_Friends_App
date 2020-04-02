2db7be8d90b433ecce586e2a935134ce2974dbd1
Harold Carter <5639624-Harrytheweird@users.noreply.gitlab.com>
2020-04-02 18:12:47 +0100

    Initial commit

---
+++ app/src/main/java/com/example/find_my_friends/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.example.find_my_friends.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
