d1333781105bd37ae4cd9140eb3a9d638db13a04
Harold Carter <5639624-Harrytheweird@users.noreply.gitlab.com>
2020-04-07 21:08:28 +0100

    group details page map view working, constasts class added.

--- app/src/main/java/com/example/find_my_friends/GroupDetailsActivity.java
+++ app/src/main/java/com/example/find_my_friends/GroupDetailsActivity.java
@@ -2,16 +2,26 @@ package com.example.find_my_friends;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
-public class GroupDetailsActivity extends AppCompatActivity {
+import static com.example.find_my_friends.util.Constants.MAPVIEW_BUNDLE_KEY;
+
+public class GroupDetailsActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private MapView mapView;
+    private LatLng groupLatLng = new LatLng(0,0);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +32,76 @@ public class GroupDetailsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar supportBar = getSupportActionBar();
 
-        if(supportBar != null) {
+        if (supportBar != null) {
             getSupportActionBar().setTitle("");
+            //default theme sets the title incorrectly
             getSupportActionBar().setIcon(R.drawable.svg_back_arrow_primary);
         }
 
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
 
+        mapView = (MapView) findViewById(R.id.group_details_map_view);
+        mapView.onCreate(mapViewBundle);
 
+        mapView.getMapAsync(this);
     }
 
 
 
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions().position(groupLatLng).title("marker"));
+    }
+
+    @Override
+    protected void onStart() {
+        mapView.onStart();
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        mapView.onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
 }
---
+++ app/src/main/java/com/example/find_my_friends/util/Constants.java
@@ -0,0 +1,5 @@
+package com.example.find_my_friends.util;
+
+public class Constants {
+    public static final String MAPVIEW_BUNDLE_KEY = "GroupDetailsMapViewKey";
+}
